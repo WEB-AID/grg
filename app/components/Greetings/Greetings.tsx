2db06c4fa079919cab3a79aa5e99be88a5f86a69
WEB-AID <web.aid.dev@gmail.com>
2024-10-24 15:08:16 +0200

    11 commit

--- app/components/Greetings/Greetings.tsx
+++ app/components/Greetings/Greetings.tsx
@@ -4,7 +4,7 @@ export default function Greetings() {
     return (
         <div className="md:overflow-hidden">
             <div className="flex flex-col md:flex-row md:w-full md:items-center">
-                <article className="flex flex-center w-60vh items-center flex-col max-[767px]:mx-auto md:pl-24 md:w-1/2">
+                <article className="flex flex-center items-center flex-col px-12 md:px-0 max-[767px]:mx-auto md:pl-24 md:w-1/2">
                     <h1 className="text-xl mb-5">Georgian House</h1>
                     12nam atque! Ea soluta excepturi labore, quis vero ipsum illum repellendus animi,
                     fugit ratione tenetur fuga, consequatur blanditiis at aperiam magni dolores velit
